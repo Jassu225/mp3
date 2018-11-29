83ac94969f2a4b67cf335ca67603d08567317050
Jaswanth Sai Sattenapalli <jaswanthsaisattenapalli@gmail.com>
2018-11-29 18:10:53 +0530

    Added environment vars

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
 // const apiKey = 'ad6d1b7996912f36983366d6dcabb471';
 // const country = 'india';
 const apiRootURL = 'http://localhost:7991';
+if(process.env.ENVIRONMENT == "PRODUCTION") {
+    apiRootURL = 'https://music-rest-api.herokuapp.com/';
+}
+
 const config = {
     apiRootURL,
     uploadSongURL: `${apiRootURL}/uploadSong`,
